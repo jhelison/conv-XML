81636b1bf849aa26c16890e2129318f7119fedec
Aiack <jhelisong@gmail.com>
2020-07-24 12:02:34 -0300

    Small changes

--- src/Config.py
+++ src/Config.py
@@ -6,30 +6,28 @@ class Config():
         if not os.path.isdir('./config'):
             os.makedirs('./config')
             
-        self.f = open('./config/config.json', 'w+')
-        
-        try:
-            self.config = json.load(self.f)
-        except:
-            self.config = {}
-            
-        if not self.config.keys():
-            self.init()
+        with open('./config/config.json', 'w+') as f:
+            text = f.read()
+            print('text is: ', text)
+            try:
+                self.config = json.loads(text)
+                f.close()
+            except:
+                self.init()
   
     def init(self):
         self.config['cert_path'] = ""
         self.config['input_folder'] = ""
         self.config['output_folder'] = ""
         self.config['cert_password'] = ""
-        
-        json.dump(self.config, self.f)
-        
-        self.f.close()
+           
+        with open('./config/config.json', 'w') as f:     
+            json.dump(self.config, f)
+            f.close()
             
     def get(self, key):
         with open('./config/config.json', 'r') as f:
             text = f.read()
-            print(text)
             config = json.loads(text)
             f.close()
         
@@ -38,4 +36,6 @@ class Config():
     def save(self, key, value):
         self.config[key] = value
         
-        json.dump(self.config, self.f)
+        with open('./config/config.json', 'w') as f:
+            json.dump(self.config, f)
+            f.close()
